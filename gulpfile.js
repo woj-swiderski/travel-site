f1053b2bb2a42b244c7f4f42d67f49a86278376c
Wojciech Świderski <woj.swiderski@gmail.com>
2019-05-14 22:01:26 +0200

    some gulp add-ons

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ const
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
-  nested = require('postcss-nested')
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import'),
+  browserSync = require('browser-sync').create()
 
 
 gulp.task('default', () => {
@@ -19,18 +21,30 @@ gulp.task('html', () => {
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'))
   }
 )
 
 gulp.task('watch',  () => {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  })
+
   watch('./app/index.html', () => {
-    gulp.start('html')
+    browserSync.reload();
     })
 
   watch('./app/assets/styles/**/*.css', () => {
-    gulp.start('styles')
+    gulp.start('cssInject')
   })
 
+})
+
+gulp.task('cssInject', ['styles'], () => {
+  return gulp.src('./app/temp/styles/styles.css').pipe(browserSync.stream())
 })
